75f287be1bc99ce7678558b7f228ed18af631310
AASIFKAMAL <kamalaasif48@gmail.com>
2025-06-18 16:52:24 +0530

    Fix dependencies and vulnerabilities

--- app.js
+++ app.js
@@ -99,16 +99,6 @@ app.use(( req, res, next) =>  {
 });
 
 
-// app.get("/demouser", async (req, res) => {
-//     let fakeUser = new User({
-//         email: "student@gmail.com",
-//         username: "delta-student",
-//     });
-
-//     let registeredUser = await User.register(fakeUser, "helloworld");
-//     res.send(registeredUser);
-// });
-
 
 
 app.use("/listings", listingRouter);
